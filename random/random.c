366b218d9080a07d88938fc57168326bad6520c3
Maxwell Hinson <maxwellhinson@gmail.com>
2014-05-30 11:36:00 -0700

    fixed buffer alloc issue

--- random/random.c
+++ random/random.c
@@ -182,7 +182,8 @@ int main(int argc,char *argv[])
     // Read the system_best.txt graph size
     char* buf = (char*) malloc(10*sizeof(int));
     while(fgets(buf, sizeof buf, fp) != NULL) {
-      graphSize = buf;
+      //graphSize = buf;
+      memcpy(&graphSize, buf, sizeof(graphSize));
     }
 
     // Read the system_best.txt clique count
