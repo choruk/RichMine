1bd769dbb92ad120e72b997015c3fd3ae5e53f46
Maxwell Hinson <maxwellhinson@gmail.com>
2014-05-31 16:31:43 -0700

    set batch job for 16 hours

--- random/launch.sh
+++ random/launch.sh
@@ -3,7 +3,7 @@
 #SBATCH -o stdout.%j
 #SBATCH -e stderr.%j
 #SBATCH -n 1
-#SBATCH -t 00:30:00
+#SBATCH -t 16:00:00
 #SBATCH -p normal
 #SBATCH --mail-user=mhinson@umail.ucsb.edu
 #SBATCH --mail-type=all
